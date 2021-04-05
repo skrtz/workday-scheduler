53ffe0ae1acd840be15fc92cc398e09757fc401a
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-04-04 22:19:37 -0400

    add custom save button

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function getDailyInput(){
     var input6 = localStorage.getItem('6pm');
     var input7 = localStorage.getItem('7pm');
     var input8 = localStorage.getItem('8pm');
-    var input9 = localStorage.getItem('9pm');
+    var input9pm = localStorage.getItem('9pm');
     var input10pm = localStorage.getItem('10pm');
     var input11pm = localStorage.getItem('11pm');
 
@@ -32,7 +32,7 @@ function getDailyInput(){
     $('#txt6').text(input6);
     $('#txt7').text(input7);
     $('#txt8').text(input8);
-    $('#txt9').text(input9);
+    $('#txt9').text(input9pm);
     $('#txt10pm').text(input10pm);
     $('#txt11pm').text(input11pm);
 };
@@ -45,3 +45,92 @@ $('.saveBtn').on('click', function(){
 
     localStorage.setItem(time, input);
 });
+
+// timeblock color code
+var currentTime = moment().hour();
+$('.description').addClass('past')
+
+    if (currentTime === 9) {
+        $('#txt9').addClass('present');
+    } else if (currentTime < 9) {
+        $('#txt9').addClass('future');
+    }
+
+    if (currentTime === 10){
+        $('#txt10').addClass('present');
+    } else if (currentTime < 10) {
+        $('#txt10').addClass('future');
+    }
+
+    if (currentTime === 12) {
+        $('#txt12').addClass('present');
+    } else if (currentTime < 12) {
+        $('#txt12').addClass('future');
+    }
+
+    if (currentTime === 13){
+        $('#txt1').addClass('present');
+    } else if (currentTime < 13){
+        $('#txt8').addClass('future');
+    }
+
+    if (currentTime === 14) {
+        $('#txt2').addClass('present');
+    } else if (currentTime < 14) {
+        $('#txt2').addClass('future');
+    }
+
+    if (currentTime === 15){
+        $('#txt3').addClass('present');
+    } else if (currentTime < 15) {
+        $('#txt3').addClass('future');
+    }
+
+    if (currentTime === 16) {
+        $('#txt4').addClass('present');
+    } else if (currentTime < 16) {
+        $('#txt4').addClass('future');
+    }
+
+    if (currentTime === 17) {
+        $('#txt5').addClass('present');
+    } else if (currentTime < 17) {
+        $('#txt5').addClass('future');
+    }
+
+    if (currentTime === 18) {
+        $('#txt6').addClass('present');
+    } else if (currentTime < 18){
+        $('#txt6').addClass('future');
+    }
+
+    if (currentTime === 19) {
+        $('#txt7').addClass('present');
+    } else if (currentTime < 19) {
+        $('#txt7').addClass('future');
+    }
+
+    if (currentTime === 20){
+        $('#txt8').addClass('present');
+    } else if (currentTime < 21){
+        $('#txt8').addClass('future');
+    }
+
+    if (currentTime === 21){
+        $('#txt9pm').addClass('present');
+    } else if (currentTime < 21){
+        $('#txt9pm').addClass('future');
+    }
+
+    if (currentTime === 22){
+        $('#txt10pm').addClass('present');
+    } else if (currentTime < 22){
+        $('#txt10pm').addClass('future');
+    }
+
+    if (currentTime === 23) {
+        $('#txt11pm').addClass('present');
+    } else if (currentTime < 23) {
+        $('#txt11pm').addClass('future');
+    }
+
